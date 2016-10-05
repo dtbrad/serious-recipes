713dd021aab0dd69e3455f81ba7292a068563289
Daniel Brad <danieltbrad@gmail.com>
2016-10-05 13:08:32 -0700

    created greeting and stubbed out menu methods in cli.rb

--- lib/serious_recipes/cli.rb
+++ lib/serious_recipes/cli.rb
@@ -1,7 +1,43 @@
 class SeriousRecipes::CLI
 
   def call
-    puts "Welcome to Serious Recipes"
+    greeting
+    menu
   end
+
+
+  def greeting
+    puts ""
+    puts ""
+    puts "                     WELCOME TO SERIOUS RECIPES."
+    puts ""
+    puts "This gem allows you to view and search by ingredient for the latest recipes from the Serious Eats website."
+  end
+
+  def menu
+  input=nil
+  puts ""
+  puts ""
+  puts "Initial Menu"
+  puts ""
+  puts "Please select from the following options:"
+  puts ""
+  puts "Type list to list Recipes."
+  puts "Type search to search for a recipe by ingredient."
+  puts "Type exit to leave the program"
+  puts ""
+  input=gets.strip.downcase
+
+  if input == "list"
+    puts "you selected 'list'"
+  elsif input == "search"
+    puts "you selected 'search'"
+  elsif input == "exit"
+  else
+    puts ""
+    puts "I did not understand your input. Please try again"
+    menu
+  end
+end
 
 end
